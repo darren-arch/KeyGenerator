f3f2a154ccf9fea4daac467d0506cf9a8faa3180
darren-arch <darrenverhage@gmail.com>
2025-07-15 16:59:53 -0500

    working on the frontend of the app:

--- keys.py
+++ keys.py
@@ -1,12 +1,23 @@
 import tkinter as tk
+from tkinter import ttk
 
 root = tk.Tk()
 root.title('AvaLAN Key Generator')
+root.geometry("500x500")
 
+label = tk.Label(root, text="Choose your USB:")
+label.pack(pady=10)
+
+combo_box = ttk.Combobox(root, values=["USB 1", "USB 2"])
+combo_box.pack(pady=10)
+
+#label
+
+button = tk.Button(root, text="")
 
 root.mainloop()
 
 
 
-with open('key.cfg', 'w') as file:
-    file.write(key)
+#with open('key.cfg', 'w') as file:
+#    file.write(key)

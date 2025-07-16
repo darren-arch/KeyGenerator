c9bf351701d0894522d3cc82a428269d87aabd94
darren-arch <darrenverhage@gmail.com>
2025-07-16 12:54:44 -0700

    updated get_usb_dirves function

--- keys.py
+++ keys.py
@@ -35,15 +35,31 @@ def click():
         file.write(f"{firstfour},{lastfour}")
 
 def get_usb_drives():
+    #creates an array to store the usb info on the computer 
     drive_list = []
+    #gets a binary string of what drives are active
+    #ex: 00000000000000000000011100
+    # 0 is off and 1 is on
     bitmask = ctypes.windll.kernel32.GetLogicalDrives()
+    #print(f"Bitmask: {bin(bitmask)[2:].zfill(26)}")
+    #loops through each uppercase letter in the alphabet
     for letter in string.ascii_uppercase:
+        print(f"Letter: {letter}")
+        print(f"bitmask & 1: {bitmask & 1}")
+        # for each uppercase letter it checks to see if the drive is active by checking the smallest bit
+        # for 11100 it would check the 0 on the right
         if bitmask & 1:
+            #if true it makes the drive match the current letter
             drive = f"{letter}:/"
+            #then it finds the type of drive, such as removable, internal, etc
             type = ctypes.windll.kernel32.GetDriveTypeW(drive)
+            print(type)
+            #if the drive is a removable drive (type 2) then it adds that drive to the drive array
             if type == 2:  # DRIVE_REMOVABLE
                 drive_list.append(drive)
+        # this removes the smalles bit so for 11100 it would remove the right most 0
         bitmask >>= 1
+    #returns the array
     return drive_list
 
 print("USB drives:", get_usb_drives())
